4bdfd982b3d0c9633c8b1572b1cc0eacc82de26f
param <param@desktop.com>
2021-06-04 21:26:42 +0530

    Src Folder Create

---
+++ lib/src/text-dropdown.dart
@@ -0,0 +1,59 @@
+
+ 
+import 'package:dropdown_plus/dropdown_plus.dart';
+import 'package:flutter/material.dart';
+
+/// Simple dorpdown whith plain text as a dropdown items.
+class TextDropdown extends StatelessWidget {
+  final List<String> options;
+  final InputDecoration? decoration;
+  final ValueNotifier<String>? controller;
+  final void Function(String item)? onChanged;
+  final void Function(String?)? onSaved;
+  final String? Function(String?)? validator;
+  final bool Function(String item, String str)? filterFn;
+  final Future<List<String>> Function(String str)? findFn;
+  final double? dropdownHeight;
+
+  TextDropdown({
+    Key? key,
+    required this.options,
+    this.decoration,
+    this.onSaved,
+    this.controller,
+    this.onChanged,
+    this.validator,
+    this.findFn,
+    this.filterFn,
+    this.dropdownHeight,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dropdown<String>(
+      decoration: decoration,
+      onSaved: onSaved,
+      controller: controller,
+      onChanged: onChanged,
+      validator: validator,
+      dropdownHeight: dropdownHeight,
+      displayItemFn: (dynamic str) => Text(
+        str ?? '',
+        style: TextStyle(fontSize: 16),
+      ),
+      findFn: findFn ?? (dynamic str) async => options,
+      filterFn: filterFn ??
+          (dynamic item, str) =>
+              item.toLowerCase().indexOf(str.toLowerCase()) >= 0,
+      dropdownItemFn: (dynamic item, position, focushed, selected, onTap) =>
+          ListTile(
+        title: Text(
+          item,
+          style: TextStyle(color: selected ? Colors.blue : Colors.black87),
+        ),
+        tileColor: focushed ? Color.fromARGB(10, 0, 0, 0) : Colors.transparent,
+        onTap: onTap,
+      ),
+    );
+  }
+}
